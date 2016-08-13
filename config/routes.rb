bfbecaf44f66c4bf2f3c462d6bceb147849ae79f
Vitaly Liber <liber@sibext.com>
2016-08-13 18:37:58 +0700

    added styles for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'homes#index'
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  resources :orders
-  resources :calendars
+  authenticate :user do
+    root 'homes#index'
+    resources :orders
+    resources :calendars
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
