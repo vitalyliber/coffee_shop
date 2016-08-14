37d3e73d5884695e1c789a1e6c8bffeda62736eb
Vitaly Liber <liber@sibext.com>
2016-08-14 21:38:26 +0700

    remove junk files

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,31 +1,15 @@
 class OrdersController < ApplicationController
   load_and_authorize_resource
-  before_action :set_order, only: [:show, :edit, :update, :destroy]
   before_action :has_point?, only: :index
 
-  # GET /orders
-  # GET /orders.json
   def index
     @products = Product.all
     @point = Point.find_by(id: params[:point])
   end
 
-  # GET /orders/1
-  # GET /orders/1.json
   def show
   end
 
-  # GET /orders/new
-  def new
-    @order = Order.new
-  end
-
-  # GET /orders/1/edit
-  def edit
-  end
-
-  # POST /orders
-  # POST /orders.json
   def create
     @order = Order.create(order_params)
     repeat_products = JSON.parse(params[:repeat_products], symbolize_names: :true)
@@ -49,37 +33,7 @@ class OrdersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /orders/1
-  # PATCH/PUT /orders/1.json
-  def update
-    respond_to do |format|
-      if @order.update(order_params)
-        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
-        format.json { render :show, status: :ok, location: @order }
-      else
-        format.html { render :edit }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /orders/1
-  # DELETE /orders/1.json
-  def destroy
-    @order.destroy
-    respond_to do |format|
-      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_order
-      @order = Order.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
       params.require(:order).permit(:title, :cost_price, :point_id)
     end
