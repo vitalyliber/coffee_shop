1dac6940730c9141e34a6d9bbad5d898f3f2f815
Vitaly Liber <liber@sibext.com>
2016-08-17 23:11:20 +0700

    minor changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :rvm_ruby_version, '2.3.1@coffee-shop'
 #add log file to shared folder
 set :linked_dirs, %w{log tmp}
 
-set :puma_conf, "home/publisher/coffee_shop/shared/puma.rb"
+set :puma_conf, "/home/publisher/coffee_shop/shared/puma.rb"
 
 
 namespace :deploy do
