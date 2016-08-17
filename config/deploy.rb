5929ab9978068390faf2b0c2b9f33d5df3ee8a2e
Vitaly Liber <liber@sibext.com>
2016-08-17 23:01:43 +0700

    updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,25 @@ set :rvm_ruby_version, '2.3.1@coffee-shop'
 #add log file to shared folder
 set :linked_dirs, %w{log tmp}
 
+set :puma_user, fetch(:user)
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
+set :puma_default_control_app, "unix://#{shared_path}/tmp/sockets/pumactl.sock"
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_access.log"
+set :puma_error_log, "#{shared_path}/log/puma_error.log"
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :puma_threads, [0, 16]
+set :puma_workers, 0
+set :puma_worker_timeout, nil
+set :puma_init_active_record, false
+set :puma_preload_app, false
+set :puma_plugins, []  #accept array of plugins
+set :nginx_use_ssl, false
+
 namespace :deploy do
 
   desc 'Restart application'
