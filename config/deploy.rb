d8f2fc7247d40329bfb6311e2ad7006093b1f0ee
Vitaly Liber <liber@sibext.com>
2016-08-17 21:52:51 +0700

    update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,38 @@
+
+
 # config valid only for current version of Capistrano
 lock '3.6.0'
 
 set :application, 'coffee_shop'
 set :repo_url, 'git@github.com:vitalyliber/coffee_shop.git'
+
+set :rvm_ruby_version, '2.3.1@coffee-shop'
+
+#add log file to shared folder
+set :linked_dirs, %w{log}
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
