43c3cd0dbbe92356498aa9c8a239ee4f19cf52e1
Vitaly Liber <liber@sibext.com>
2016-08-18 23:13:19 +0700

    change ruby v

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.6.0'
 set :application, 'coffee_shop'
 set :repo_url, 'git@github.com:vitalyliber/coffee_shop.git'
 
-set :rvm_ruby_version, '2.3.1@coffee-shop'
+set :rvm_ruby_version, '2.3.0@coffee-shop'
 
 #add log file to shared folder
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
