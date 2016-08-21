68a3b40f9f799a2e794b9fb23b3bade429684902
Vitaly Liber <liber@sibext.com>
2016-08-22 01:22:05 +0700

    change deploy dir

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,7 +4,7 @@ role :db,  %w{publisher@coffee-shop-staging.toel.ru}
 
 server 'coffee-shop-staging.toel.ru', user: 'publisher', roles: %w{web app}, primary: true
 set :rails_env, 'staging'
-set :deploy_to, "/home/publisher/coffee_shop"
+set :deploy_to, "/home/publisher/coffee_shop_staging"
 set :branch, ENV['BRANCH'] || 'master'
 
 set :rvm_ruby_version, '2.3.1@coffee-shop-staging'
