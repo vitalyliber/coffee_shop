415c2b43c0b4ccfa24d5184bdc960d6f53b9f086
Vitaly Liber <liber@sibext.com>
2016-08-22 22:36:03 +0700

    AddValidation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   resourcify
-  validates :title, :price, :ml
+  validates :title, :price, :ml, presence: :true
 
   has_many :order_lists
   has_many :orders, through: :order_lists
