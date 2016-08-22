055ba0920a37ca5bb1502c4eb3eecd61a36c2e32
Vitaly Liber <liber@sibext.com>
2016-08-22 22:21:14 +0700

    AddValidation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ApplicationRecord
   resourcify
+  validates :products, :cost_price, :point_id, presence: :true
+
   has_many :order_lists
   has_many :products, through: :order_lists
   belongs_to :point
--- app/models/order_list.rb
+++ app/models/order_list.rb
@@ -1,4 +1,5 @@
 class OrderList < ApplicationRecord
+  validates :order, :product, presence: :true
   belongs_to :order
   belongs_to :product
 end
