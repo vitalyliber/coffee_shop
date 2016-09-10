be0de347bf15c631c8cbad54373e7badb75881a9
Vitaly Liber <liber@sibext.com>
2016-09-10 23:16:30 +0700

    added Product Management

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,52 @@
 class ProductsController < ApplicationController
+  before_action :find_point
+  before_action :find_product, only: [:update, :edit]
+  include ProductsHelper
+
   def index
-    @point = Point.find_by(id: params[:point_id])
     @products = ProductList.find_by(point: @point).products
   end
 
   def edit
+  end
+
+  def show
+    redirect_to point_products_path(@point)
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to point_products_path(@point)
+    else
+      render :edit
+    end
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to point_products_path(@point)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :ml, :price)
+  end
+
+  def find_product
     @product = Product.find_by(id: params[:id])
+  end
+
+  def find_point
+    @point = Point.find_by(id: params[:point_id])
   end
 end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,21 @@
 module ProductsHelper
+  def path_for_form
+    if params[:action] == 'new'
+      point_products_path(@point)
+    elsif params[:action] == 'create'
+      point_products_path(@point)
+    elsif params[:action] == 'edit'
+      point_product_path(@point, @product)
+    elsif params[:action] == 'update'
+      point_product_path(@point, @product)
+    end
+  end
+
+  def submit_for_form
+    if params[:action] == 'new'
+      'CREATE PRODUCT'
+    elsif params[:action] == 'edit'
+      'SAVE PRODUCT'
+    end
+  end
 end
