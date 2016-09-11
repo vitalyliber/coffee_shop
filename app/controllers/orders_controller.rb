a32445eb94d12791c25b8a45bea3296491f39274
Vitaly Liber <liber@sibext.com>
2016-09-12 01:05:03 +0700

    added statistics without styles =)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,8 +3,18 @@ class OrdersController < ApplicationController
   before_action :find_point
   include OrdersHelper
 
+  def index
+    @day_sales = @point.day_sales.where(status: :closed, user: current_user).order(created_at: :desc)
+  end
+
   def day_sales
-    @day_sales = @point.day_sales.find_by(status: :opened, user: current_user)
+    day_sale_id = params[:day_sale]
+
+    if day_sale_id.present?
+      @day_sales = @point.day_sales.find_by(id: day_sale_id)
+    else
+      @day_sales = @point.day_sales.find_by(status: :opened, user: current_user)
+    end
 
     @orders = @day_sales.orders.current_sales(@day_sales.start).order(created_at: :desc)
   end
