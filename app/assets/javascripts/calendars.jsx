38fc28177214931f00ee445714250ca4c6191af2
Vitaly Liber <liber@sibext.com>
2016-09-15 00:19:09 +0700

    improve day sale cards

--- app/assets/javascripts/calendars.jsx
+++ app/assets/javascripts/calendars.jsx
@@ -4,4 +4,10 @@ document.addEventListener("turbolinks:load", function() {
     localDate = moment(DateUtc).local();
     this.textContent = localDate.format('HH:mm:ss');
   });
+
+  $('.datetime').each(function() {
+    DateUtc = moment.utc( $(this).attr('val') );
+    localDate = moment(DateUtc).local();
+    this.textContent = localDate.format('Do MMM HH:mm');
+  });
 });
